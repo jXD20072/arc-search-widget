db8deea628c69b9c626b8df72fed6140bae89454
jXD20072 <116370539+jXD20072@users.noreply.github.com>
2024-10-20 10:08:34 +0200

    Minor UI tweaks

--- app/src/main/java/com/jXD20072/arcsearchwidget/arcsearchwidget.kt
+++ app/src/main/java/com/jXD20072/arcsearchwidget/arcsearchwidget.kt
@@ -20,12 +20,11 @@ import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.ContentScale
 import androidx.glance.layout.Row
-import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.height
 import androidx.glance.layout.padding
-import androidx.glance.layout.width
+import androidx.glance.layout.size
 import androidx.glance.text.FontFamily
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
@@ -41,75 +40,82 @@ class arcsearchwidget : GlanceAppWidget() {
         // operations.
 
         provideContent {
-            // create your AppWidget here
             MyContent(context)
         }
     }
-
-
 
     @Composable
     private fun MyContent(context: Context) {
         Box(
             modifier = GlanceModifier
                 .fillMaxWidth()
-                .height(60.dp)
+                .height(52.dp)
                 .cornerRadius(40.dp),
         ) {
 
-            Image(provider = ImageProvider(R.drawable.background),
-                contentDescription = "bg-image", contentScale = ContentScale.Crop,
+            Image(
+                provider = ImageProvider(R.drawable.background),
+                contentDescription = "bg-image",
+                contentScale = ContentScale.Crop,
                 modifier = GlanceModifier
                     .fillMaxWidth()
             )
 
             Row() {
-                Box(modifier = GlanceModifier
-                    .fillMaxHeight()
-                    .width(70.dp),
+                Box(
+                    modifier = GlanceModifier
+                        .size(52.dp)
+                        .padding(start = 10.dp),
                     contentAlignment = Alignment(
                         horizontal = Alignment.CenterHorizontally,
                         vertical =  Alignment.CenterVertically)
                 ) {
-                    Image(provider = ImageProvider(R.drawable.arc_logo),
+                    Image(
+                        provider = ImageProvider(R.drawable.arc_logo),
                         contentDescription = "arc-logo",
                         modifier = GlanceModifier
-                            .fillMaxSize()
-                            .padding(15.dp)
+                            .size(38.dp)
                     )
                 }
 
-                Box(modifier = GlanceModifier
-                    .fillMaxSize()
-                    .padding(end = 10.dp),
+                Box(
+                    modifier = GlanceModifier
+                        .fillMaxSize()
+                        .padding(8.dp),
                     contentAlignment = Alignment(
                         horizontal = Alignment.Start,
                         vertical = Alignment.CenterVertically)
                 ) {
-                    Box(modifier = GlanceModifier
-                        .fillMaxWidth()
-                        .height(40.dp)
-                        .background(Color(236, 236, 236))
-                        .cornerRadius(30.dp),
+                    Box(
                         contentAlignment = Alignment(
                             horizontal = Alignment.Start,
-                            vertical = Alignment.CenterVertically)
+                            vertical = Alignment.CenterVertically
+                        ),
+                        modifier = GlanceModifier
+                            .fillMaxSize()
+                            .background(Color(236, 236, 236))
+                            .cornerRadius(30.dp)
                     ) {}
 
-                    Text(text = "Search", style = TextStyle(
-                        color = ColorProvider(Color(164, 164, 164, 255)),
-                        fontFamily = FontFamily("Inter"),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Medium,
-                        textAlign = TextAlign.Center
-                    ), modifier = GlanceModifier
-                        .padding(start = 15.dp)
+                    Text(
+                        text = "Search",
+                        style = TextStyle(
+                            color = ColorProvider(Color(164, 164, 164, 255)),
+                            fontFamily = FontFamily("Inter"),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium,
+                            textAlign = TextAlign.Center
+                        ),
+                        modifier = GlanceModifier
+                            .padding(start = 15.dp)
                     )
                 }
             }
 
-            Button("", modifier = GlanceModifier
-                .fillMaxSize(),
+            Button(
+                text = "",
+                modifier = GlanceModifier
+                    .fillMaxSize(),
                 colors = ButtonDefaults.buttonColors(
                     backgroundColor = ColorProvider(Color.Transparent),
                     contentColor = ColorProvider(Color.Transparent)
@@ -125,7 +131,7 @@ class arcsearchwidget : GlanceAppWidget() {
 fun openArc(context: Context) {
     val intent = context.packageManager.getLaunchIntentForPackage("company.thebrowser.arc")
     if (intent != null) {
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) // Ensure it's a new task
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         context.startActivity(intent)
     }
 }
